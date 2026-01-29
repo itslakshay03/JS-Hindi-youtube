9701807b3a1d946280f29028449a368ff5ca46b6
itslakshay03 <lakshayanand2004@gmail.com>
2026-01-29 14:12:47 +0000

    basic about comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,10 +14,35 @@
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLogged = 1
-let booleanIsLoggedIn = Boolean(isLogged)
-console.log(booleanIsLoggedIn);
+// let isLogged = 1
+// let booleanIsLoggedIn = Boolean(isLogged)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "lakshay" => true
+
+
+// ************** Operations ****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " lakshay"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// while comparing js convert null as 0 and then compare but == as different
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// in case of undefined it will always show false.
+
+// === strict check means checking data_types also.
+// console.log("2" === 2);
