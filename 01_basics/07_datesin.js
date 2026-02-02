b066e67b9a466fca9f764a98061746118ba4460b
itslakshay03 <lakshayanand2004@gmail.com>
2026-02-02 15:33:45 +0000

    Dates

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,22 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString()); // isme date ke sath time bhi ata h
+// console.log(typeof myDate); // output will be as OBJECT
+
+// let myCreatedDate = new Date(2025,8,3)
+let myCreatedDate = new Date("2025-02-02")
+console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
