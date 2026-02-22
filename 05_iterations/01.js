74ef5d18419b8e9d7f838d967fb9a0931fa791f3
itslakshay03 <lakshayanand2004@gmail.com>
2026-02-22 09:15:23 +0000

    for loop, break and continue

---
+++ 05_iterations/01.js
@@ -0,0 +1,34 @@
+// // for
+
+// for (let index = 1; index <= 10; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+
+
+// let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+
+// for(let i=0; i<myArray.length; i++){
+//     const element = myArray[i];
+//     console.log(element);
+// }
+
+
+// break and continue  -------->
+
+// for(let i=1; i<=20; i++){
+//     if(i==5){
+//         console.log(`detected 5`);
+//         break;
+//     }
+//     console.log(`value of i is ${i}`);
+// }
+
+// for(let i=1; i<=20; i++){
+//     if(i==5){
+//         console.log(`detected 5`);
+//         continue;
+//     }
+//     console.log(`value of i is ${i}`);
+// }
