8c0dcb1706fe50e862d92d59c1e294f033c204db
itslakshay03 <lakshayanand2004@gmail.com>
2026-02-22 09:23:44 +0000

    while loop

---
+++ 05_iterations/02.js
@@ -0,0 +1,13 @@
+// while
+// let index = 0
+// while(index<=10){
+//     console.log(`value of index is ${index}`);
+//     index=index+2
+// }
+
+let myArray = ['flash', 'batman', 'superman']
+let i=0
+while(i<myArray.length){
+    console.log(`value is ${myArray[i]}`);
+    i++;
+}
